a734aafb1c46c38ec063b1afba55a981cc446606
shuoshan <ss17564@nyu.edu>
2023-10-25 16:54:09 -0400

    update

--- dom/sketch.js
+++ dom/sketch.js
@@ -1,7 +1,17 @@
+let slider;
+let cp;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  slider = createSlider(10, width, 10);
+  slider.position(100,100);
+
+  cp = createColorPicker("gold");
+  cp.position(100,300);
 }
 
 function draw() {
-  background(255, 55, 155);
+  background(cp.color());
+  let currentVal = slider.value();
+  ellipse(currentVal, height/2, 30, 30);
 }
--- sound/sketch.js
+++ sound/sketch.js
@@ -1,40 +1,27 @@
 let song;
-let song1;
+let samples;
 
 function preload() {
-  song = loadSound("./epic-hip-pop.mp3");
-  song1 = loadSound("./funny-tango.mp3");
+  // song = loadSound("./epic-hip-pop.mp3");
+  song = loadSound("./funny-tango.mp3");
 }
 
-function nextsong(){
-  song1.play();
-}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  samples = song.getPeaks();
   print(song.duration());
   song.playmode("restart");
 }
 
 function draw() {
-  if (song.isPlaying()) {
-    background(20, 220, 120);
-  } else if (song.isPaused()){
-    background(20,120,220);
-  } else {
-    background(220,20,120);
-  }
-}
-
-function mouseClicked(){
-  song.play();
-}
-
-function keyReleased(){
-  if (key == "s"){
-    song.stop();
-  } else if (key == "p"){
-    if (song.isPlaying()) {
-
+  push();
+  translate(0, height/2);
+    for(let i = 1; i < width; i++){
+      let x0 = i - 1;
+      let y0 = map(samples[x0], -1, 1, -height/2, height/2);
+      let y1 = map(samples[i], -1, 1, -height/2, height/2);
+      line(x0, y0, i, y1);
     }
-  }
+    pop();
 }
