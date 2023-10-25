a897a3df6d0e131ed699a14438a8d0fb048fea8a
shuoshan <ss17564@nyu.edu>
2023-10-25 16:20:39 -0400

    update 1

--- sound/sketch.js
+++ sound/sketch.js
@@ -1,14 +1,40 @@
 let song;
+let song1;
 
 function preload() {
-  song = loadSound("./funny-tango.mp3");
+  song = loadSound("./epic-hip-pop.mp3");
+  song1 = loadSound("./funny-tango.mp3");
 }
 
+function nextsong(){
+  song1.play();
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
   print(song.duration());
+  song.playmode("restart");
 }
 
 function draw() {
-  background(220, 20, 120);
+  if (song.isPlaying()) {
+    background(20, 220, 120);
+  } else if (song.isPaused()){
+    background(20,120,220);
+  } else {
+    background(220,20,120);
+  }
+}
+
+function mouseClicked(){
+  song.play();
+}
+
+function keyReleased(){
+  if (key == "s"){
+    song.stop();
+  } else if (key == "p"){
+    if (song.isPlaying()) {
+
+    }
+  }
 }
